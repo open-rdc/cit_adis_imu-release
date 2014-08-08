ef4ecd7018dae92358dfbf12c5e4c43717d4b8b3
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2014-08-08 21:47:18 +0900

    Implemented the t_frog driver

---
+++ t_frog_driver/src/t_frog_driver_node.cpp
@@ -0,0 +1,85 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+
+#include <controller_manager/controller_manager.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <realtime_tools/realtime_buffer.h>
+
+class TFrog : public hardware_interface::RobotHW
+{
+public:
+  TFrog()
+  {
+    pos_[0] = 0.0; pos_[1] = 0.0;
+    vel_[0] = 0.0; vel_[1] = 0.0;
+    eff_[0] = 0.0; eff_[1] = 0.0;
+    cmd_[0] = 0.0; cmd_[1] = 0.0;
+
+    hardware_interface::JointStateHandle state_handle_1("right_wheel_hinge", &pos_[0], &vel_[0], &eff_[0]);
+    jnt_state_interface_.registerHandle(state_handle_1);
+
+    hardware_interface::JointStateHandle state_handle_2("left_wheel_hinge", &pos_[1], &vel_[1], &eff_[1]);
+    jnt_state_interface_.registerHandle(state_handle_2);
+
+    registerInterface(&jnt_state_interface_);
+
+    hardware_interface::JointHandle vel_handle_1(jnt_state_interface_.getHandle("right_wheel_hinge"), &cmd_[0]);
+    jnt_vel_interface_.registerHandle(vel_handle_1);
+
+    hardware_interface::JointHandle vel_handle_2(jnt_state_interface_.getHandle("left_wheel_hinge"), &cmd_[1]);
+    jnt_vel_interface_.registerHandle(vel_handle_2);
+
+    registerInterface(&jnt_vel_interface_);
+  }
+
+  ros::Time getTime() const {return ros::Time::now();}
+  ros::Duration getPeriod() const {return ros::Duration(0.01);}
+
+  void read()
+  {
+    ROS_INFO_STREAM("Commands for joints: " << cmd_[0] << ", " << cmd_[1]);
+  }
+
+  void write()
+  {
+    for (unsigned int i = 0; i < 2; ++i)
+    {
+      pos_[i] += vel_[i]*getPeriod().toSec(); // update position
+      vel_[i] = cmd_[i]; // might add smoothing here later
+    }
+  }
+
+private:
+  hardware_interface::JointStateInterface    jnt_state_interface_;
+  hardware_interface::VelocityJointInterface jnt_vel_interface_;
+  double cmd_[2];
+  double pos_[2];
+  double vel_[2];
+  double eff_[2];
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "t_frog");
+  ros::NodeHandle nh;
+
+  TFrog robot;
+  ROS_WARN_STREAM("period: " << robot.getPeriod().toSec());
+  controller_manager::ControllerManager cm(&robot, nh);
+
+  ros::Rate rate(1.0 / robot.getPeriod().toSec());
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  while(ros::ok())
+  {
+    robot.read();
+    cm.update(robot.getTime(), robot.getPeriod());
+    robot.write();
+    rate.sleep();
+  }
+  spinner.stop();
+
+  return 0;
+}
