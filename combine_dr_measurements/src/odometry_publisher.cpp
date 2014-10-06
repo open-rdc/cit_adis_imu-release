0bd61e4caad09df76e0f59c409f21e400e22f3b3
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2014-10-07 01:26:08 +0900

    Add the correction of the amount of movement for a mobile base

--- combine_dr_measurements/src/odometry_publisher.cpp
+++ combine_dr_measurements/src/odometry_publisher.cpp
@@ -1,5 +1,8 @@
+#include <complex>
+
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/LinearMath/Quaternion.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/Imu.h>
 
@@ -28,8 +31,9 @@ public:
     }
 
     void run(){
-        ros::Rate r(100.0);
+        ros::Rate r(50.0);
         tf::TransformBroadcaster odom_broadcaster;
+        nav_msgs::Odometry old_odom;
 
         while(nh_.ok()){
             nav_msgs::Odometry *odom = received_odom_.readFromRT();
@@ -37,8 +41,6 @@ public:
 
             if(odom && imu){
                 ros::Time time = ros::Time::now();
-                ROS_INFO_STREAM("received_odom.stamp = " << odom->header.stamp);
-                ROS_INFO_STREAM("received_imu.stamp = " << imu->header.stamp);
                 geometry_msgs::TransformStamped odom_trans;
 
                 odom->header.stamp    = odom_trans.header.stamp = time;
@@ -47,12 +49,55 @@ public:
                 
                 odom->pose.pose.orientation = odom_trans.transform.rotation = imu->orientation;
                
-                odom_trans.transform.translation.x = odom->pose.pose.position.x;
-                odom_trans.transform.translation.y = odom->pose.pose.position.y;
-                odom_trans.transform.translation.z = odom->pose.pose.position.z;
+                const double dt = (odom->header.stamp - old_odom.header.stamp).toSec();
+                if(dt < 0.0001){
+                    ROS_WARN_STREAM("dt = " << dt);
+                    old_odom.pose.pose.orientation = odom->pose.pose.orientation;
+                    old_odom.pose.pose.position = odom->pose.pose.position;
+                }else{
+                    tf::Quaternion old_q(
+                        old_odom.pose.pose.orientation.x,
+                        old_odom.pose.pose.orientation.y,
+                        old_odom.pose.pose.orientation.z,
+                        old_odom.pose.pose.orientation.w
+                    );
 
-                odom_broadcaster.sendTransform(odom_trans);
-                odom_pub_.publish(*odom);
+                    tf::Quaternion q(
+                        odom->pose.pose.orientation.x,
+                        odom->pose.pose.orientation.y,
+                        odom->pose.pose.orientation.z,
+                        odom->pose.pose.orientation.w
+                    );
+
+                    double roll, pitch, yaw, old_roll, old_pitch, old_yaw;
+                    tf::Matrix3x3(old_q).getRPY(old_roll, old_pitch, old_yaw);
+                    tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+                    const double linear = odom->twist.twist.linear.x * dt;
+                    const double angular = yaw - old_yaw;
+                    
+                    if(std::abs(angular) < 10e-3){
+                        double direction = old_yaw + angular * 0.5;
+                        ROS_INFO_STREAM("direction = " << direction);
+                        ROS_INFO_STREAM("cos = " << cos(direction));
+                        odom->pose.pose.position.x = old_odom.pose.pose.position.x + linear * cos(direction);
+                        odom->pose.pose.position.y = old_odom.pose.pose.position.y + linear * sin(direction);
+                        ROS_INFO_STREAM("odom->pose.pose.position.x = " << odom->pose.pose.position.x);
+                        ROS_INFO_STREAM("odom->pose.pose.position.y = " << odom->pose.pose.position.y);
+                    }else{
+                        const double r = linear / angular;
+                        odom->pose.pose.position.x = old_odom.pose.pose.position.x + r * (sin(yaw) - sin(old_yaw));
+                        odom->pose.pose.position.y = old_odom.pose.pose.position.y - r * (cos(yaw) - cos(old_yaw));
+                    }
+
+                    odom_trans.transform.translation.x = odom->pose.pose.position.x;
+                    odom_trans.transform.translation.y = odom->pose.pose.position.y;
+                    odom_trans.transform.translation.z = odom->pose.pose.position.z;
+
+                    odom_broadcaster.sendTransform(odom_trans);
+                    odom_pub_.publish(*odom);
+                   
+                    old_odom = *odom;
+                }
             }
             
             ros::spinOnce();
